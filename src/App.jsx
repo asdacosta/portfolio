91205004a07be45bb24320d309905f8f6dfe6ebe
asdacosta <asdacosta18@gmail.com>
2024-06-03 21:50:49 +0000

    Transfer load logic to it's component

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,11 @@ import "./reset.css";
 import "./App.css";
 import { Nav } from "./components/Nav/Nav";
 import { Load } from "./components/Load/Load";
-import { useState } from "react";
-import { useEffect } from "react";
 
 function App() {
-  const [loadDisplay, setLoadDisplay] = useState("flex");
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoadDisplay("none");
-    }, 3100);
-  }, []);
-
   return (
     <>
-      <Load displayValue={loadDisplay} />
+      <Load />
       <Nav />
     </>
   );
