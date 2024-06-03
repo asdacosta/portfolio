dcfc3751662357a86b9221926e899c84f942f53d
asdacosta <asdacosta18@gmail.com>
2024-06-03 21:51:54 +0000

    Remove lottie player when animation completes

--- src/components/Load/Load.jsx
+++ src/components/Load/Load.jsx
@@ -1,13 +1,26 @@
 import "@dotlottie/player-component";
 import loadStyles from "./Load.module.css";
+import { useEffect, useRef, useState } from "react";
 
-function Load({ displayValue }) {
+function Load() {
+  const animationRef = useRef(null);
+  const [loadDisplay, setLoadDisplay] = useState("flex");
+
+  useEffect(() => {
+    const animation = animationRef.current;
+    if (!animation) return;
+
+    animation.addEventListener("complete", () => {
+      setLoadDisplay("none");
+    });
+  }, []);
+
   return (
-    <section style={{ display: displayValue }} className={loadStyles.load}>
+    <section style={{ display: loadDisplay }} className={loadStyles.load}>
       <dotlottie-player
+        ref={animationRef}
         className={loadStyles.lottie}
         autoplay
-        loop
         mode="normal"
         src="https://raw.githubusercontent.com/asdacosta/portfolio/main/src/assets/Animation%20-%201717421594190.lottie"
         style={{ width: "400px", height: "400px" }}
