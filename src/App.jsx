0fd6ab3a79785609691fb98d9feabfd3f610646b
asdacosta <asdacosta18@gmail.com>
2024-06-03 11:51:39 +0000

    Add pre vite files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./reset.css";
+import "./App.css";
+import { Nav } from "./components/Nav";
+
+function App() {
+  return (
+    <>
+      <Nav></Nav>
+      <h1>Ace</h1>
+    </>
+  );
+}
+
+export default App;
