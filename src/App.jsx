0970d6c42261efd7967572163cebf39dcdb94b3f
asdacosta <asdacosta18@gmail.com>
2024-06-03 23:37:37 +0000

    Add about component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./reset.css";
 import "./App.css";
 import { Nav } from "./components/Nav/Nav";
 import { Load } from "./components/Load/Load";
+import { About } from "./components/About/About";
 
 function App() {
   return (
     <>
       <Load />
       <Nav />
+      <About />
     </>
   );
 }
