19c3a3414481eae17a6837051258d679cb978552
asdacosta <asdacosta18@gmail.com>
2024-06-04 21:33:32 +0000

    Trigger complete if onComplete is not undefined

--- src/components/Load/Load.jsx
+++ src/components/Load/Load.jsx
@@ -10,7 +10,9 @@ function Load({ onComplete }) {
     if (!animation) return;
 
     animation.addEventListener("complete", () => {
-      onComplete();
+      if (onComplete) {
+        onComplete();
+      }
     });
   }, []);
 
