c7ba160e1da6e1a6a34610c6b544ace644b6c1b8
asdacosta <asdacosta18@gmail.com>
2024-06-04 20:12:00 +0000

    Display other  components only after loading is complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,31 @@ import "./App.css";
 import { Nav } from "./components/Nav/Nav";
 import { Load } from "./components/Load/Load";
 import { About } from "./components/About/About";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+const loadVariants = {
+  exit: { opacity: 0, transition: { duration: 0.2 } },
+};
 
 function App() {
+  const [loadDisplay, setLoadDisplay] = useState(true);
+
+  function completeLoad() {
+    setLoadDisplay(false);
+  }
+
   return (
     <>
-      <Load />
-      <Nav />
-      <About />
+      <AnimatePresence>
+        {loadDisplay && (
+          <motion.div key="load" exit="exit" variants={loadVariants}>
+            <Load onComplete={completeLoad} />
+          </motion.div>
+        )}
+      </AnimatePresence>
+      {!loadDisplay && <Nav />}
+      {!loadDisplay && <About />}
     </>
   );
 }
