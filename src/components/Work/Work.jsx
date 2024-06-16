97b4ca7c4ae23f08c795a1f981ff35cd5a9c65b6
asdacosta <asdacosta18@gmail.com>
2024-06-16 23:33:54 +0000

    Animate works smoothly on drag

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -8,47 +8,125 @@ import battleShipImg from "../../assets/Works/battleshipWork.png";
 import memoryImg from "../../assets/Works/memoryWork.png";
 import dashboardImg from "../../assets/Works/dashBoardWork.png";
 import landingPageImg from "../../assets/Works/landingPageWork.png";
-import coffeeImg from "../../assets/Works/coffeeShopWork.jpg";
+import { useRef, useState } from "react";
+// import coffeeImg from "../../assets/Works/coffeeShopWork.jpg";
 
 function Work() {
+  const [mouseDownAt, setMouseDownAt] = useState(0);
+  const [prevPercentage, setPrevPercentage] = useState(0);
+  const [percentage, setPercentage] = useState(0);
+  const trackRef = useRef(null);
+
+  const handleOnDown = (clientX) => {
+    setMouseDownAt(clientX);
+  };
+
+  const handleOnUp = () => {
+    setMouseDownAt(0);
+    setPrevPercentage(percentage);
+  };
+
+  const handleOnMove = (clientX) => {
+    if (mouseDownAt === 0) return;
+
+    const mouseDelta = mouseDownAt - clientX;
+    const maxDelta = window.innerWidth / 2;
+
+    const speedFactor = 0.2;
+    const newPercentage = (mouseDelta / maxDelta) * -100 * speedFactor;
+
+    const nextPercentageUnconstrained = prevPercentage + newPercentage;
+    const nextPercentage = Math.max(
+      Math.min(nextPercentageUnconstrained, 0),
+      -100
+    );
+
+    setPercentage(nextPercentage);
+    animateTrackAndImages(nextPercentage);
+  };
+
+  const animateTrackAndImages = (nextPercentage) => {
+    if (trackRef.current) {
+      trackRef.current.animate(
+        {
+          transform: `translate(${nextPercentage}%, -50%)`,
+        },
+        { duration: 1200, fill: "forwards" }
+      );
+
+      const images = trackRef.current.getElementsByTagName("img");
+
+      const imagePositionMultiplier = 1; // Adjust this multiplier to control the rate
+      for (const image of images) {
+        const imageNextPercentage = nextPercentage * imagePositionMultiplier;
+        image.animate(
+          {
+            objectPosition: `${100 + imageNextPercentage}% center`,
+          },
+          { duration: 1200, fill: "forwards" }
+        );
+      }
+    }
+  };
+
+  const handleMouseDown = (event) => handleOnDown(event.clientX);
+  const handleTouchStart = (event) => handleOnDown(event.touches[0].clientX);
+  const handleMouseMove = (event) => handleOnMove(event.clientX);
+  const handleTouchMove = (event) => handleOnMove(event.touches[0].clientX);
+  const handleMouseUp = () => handleOnUp();
+  const handleTouchEnd = () => handleOnUp();
+
   return (
     <section className={workStyles.work}>
-      <section className={workStyles.workSlide}>
+      <section
+        ref={trackRef}
+        data-mouse-down-at={mouseDownAt}
+        data-prev-percentage={prevPercentage}
+        onMouseDown={handleMouseDown}
+        onTouchStart={handleTouchStart}
+        onMouseMove={handleMouseMove}
+        onTouchMove={handleTouchMove}
+        onMouseUp={handleMouseUp}
+        onTouchEnd={handleTouchEnd}
+        className={workStyles.workSlide}
+      >
         <section>
-          <img src={resumeImg} alt="Resume" />
+          <img src={resumeImg} alt="Resume" draggable="false" />
         </section>
         <section>
-          <img src={weatherImg} alt="Weather Forecast" />
+          <img src={weatherImg} alt="Weather Forecast" draggable="false" />
         </section>
         <section>
-          <img src={todoImg} alt="Todo list" />
+          <img src={todoImg} alt="Todo list" draggable="false" />
         </section>
         <section>
-          <img src={tictacImg} alt="Tic-tac-toe Game" />
+          <img src={tictacImg} alt="Tic-tac-toe Game" draggable="false" />
         </section>
         <section>
-          <img src={shopCartImg} alt="Shopping Cart" />
+          <img src={shopCartImg} alt="Shopping Cart" draggable="false" />
         </section>
         <section>
-          <img src={battleShipImg} alt="Battleship Game" />
+          <img src={battleShipImg} alt="Battleship Game" draggable="false" />
         </section>
         <section>
-          <img src={memoryImg} alt="Memory Game" />
+          <img src={memoryImg} alt="Memory Game" draggable="false" />
         </section>
         <section>
-          <img src={dashboardImg} alt="Dashboard" />
+          <img src={dashboardImg} alt="Dashboard" draggable="false" />
         </section>
         <section>
-          <img src={landingPageImg} alt="Landing Page" />
+          <img src={landingPageImg} alt="Landing Page" draggable="false" />
         </section>
         {/* <section>
           <img src={coffeeImg} alt="Coffee shop" />
         </section> */}
       </section>
-      <section className={workStyles.experience}>
+      {/* <section className={workStyles.experience}>
         <h2>Experience</h2>
-        <p>2 years</p>
-      </section>
+        <p>
+          <span>24 months</span> ~ <span>2 years</span>
+        </p>
+      </section> */}
     </section>
   );
 }
