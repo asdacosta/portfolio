18f7710879883aa9ed1bc0c96709efd0b400f8f5
asdacosta <asdacosta18@gmail.com>
2024-06-22 23:26:41 +0000

    Use states to add class that animates

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import connectStyles from "./Connect.module.css";
 import "@dotlottie/player-component";
 
 function Connect() {
+  const [focusedFields, setFocusedFields] = useState({
+    name: false,
+    motive: false,
+    mail: false,
+    note: false,
+  });
+
   return (
     <section className={connectStyles.connect}>
       <section className={connectStyles.fieldSection}>
@@ -19,29 +27,118 @@ function Connect() {
         <section className={connectStyles.allFields}>
           <div className={connectStyles.nameField}>
             <label htmlFor="name">
-              Name <span className={connectStyles.optionalField}>Optional</span>
+              Name{" "}
+              <span
+                className={`${connectStyles.optionalField} ${
+                  focusedFields.name ? connectStyles.animateFieldOption : ""
+                }`}
+              >
+                Optional
+              </span>
             </label>
-            <input type="text" name="name" id="name" required />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              onFocus={() => {
+                setFocusedFields((prev) => ({
+                  name: true,
+                  motive: false,
+                  mail: false,
+                  note: false,
+                }));
+              }}
+              onBlur={() => {
+                setFocusedFields((prev) => ({ ...prev, name: false }));
+              }}
+            />
           </div>
           <div className={connectStyles.motiveField}>
             <label htmlFor="motive">
               Motive{" "}
-              <span className={connectStyles.requiredField}>Required</span>
+              <span
+                className={`${connectStyles.requiredField} ${
+                  focusedFields.motive ? connectStyles.animateFieldOption : ""
+                }`}
+              >
+                Required
+              </span>
             </label>
-            <input type="text" name="motive" id="motive" required />
+            <input
+              type="text"
+              name="motive"
+              id="motive"
+              required
+              onFocus={() => {
+                setFocusedFields((prev) => ({
+                  name: false,
+                  motive: true,
+                  mail: false,
+                  note: false,
+                }));
+              }}
+              onBlur={() => {
+                setFocusedFields((prev) => ({ ...prev, motive: false }));
+              }}
+            />
           </div>
           <div className={connectStyles.emailField}>
             <label htmlFor="email">
               Email{" "}
-              <span className={connectStyles.requiredField}>Required</span>
+              <span
+                className={`${connectStyles.requiredField} ${
+                  focusedFields.mail ? connectStyles.animateFieldOption : ""
+                }`}
+              >
+                Required
+              </span>
             </label>
-            <input type="text" name="email" id="email" required />
+            <input
+              type="text"
+              name="email"
+              id="email"
+              required
+              onFocus={() => {
+                setFocusedFields((prev) => ({
+                  name: false,
+                  motive: false,
+                  mail: true,
+                  note: false,
+                }));
+              }}
+              onBlur={() => {
+                setFocusedFields((prev) => ({ ...prev, mail: false }));
+              }}
+            />
           </div>
           <div className={connectStyles.noteField}>
             <label htmlFor="note">
-              Note <span className={connectStyles.requiredField}>Required</span>
+              Note{" "}
+              <span
+                className={`${connectStyles.requiredField} ${
+                  focusedFields.note ? connectStyles.animateFieldOption : ""
+                }`}
+              >
+                Required
+              </span>
             </label>
-            <textarea name="note" id="note" cols="20" rows="10"></textarea>
+            <textarea
+              name="note"
+              id="note"
+              cols="20"
+              rows="10"
+              onFocus={() => {
+                setFocusedFields((prev) => ({
+                  name: false,
+                  motive: false,
+                  mail: false,
+                  note: true,
+                }));
+              }}
+              onBlur={() => {
+                setFocusedFields((prev) => ({ ...prev, note: false }));
+              }}
+            ></textarea>
           </div>
           <div>
             <button>Send</button>
