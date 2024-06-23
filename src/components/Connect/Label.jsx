e6f83edda3d15b81afba39acbc5efd154b8a8226
asdacosta <asdacosta18@gmail.com>
2024-06-23 11:39:57 +0000

    Add label component for fields

---
+++ src/components/Connect/Label.jsx
@@ -0,0 +1,48 @@
+import Typing from "react-typing-effect";
+import connectStyles from "./Connect.module.css";
+
+function Label({
+  nonEmptyField,
+  focusedField,
+  inputLabel,
+  typingText,
+  required,
+  id,
+}) {
+  return (
+    <label htmlFor={id}>
+      {inputLabel}{" "}
+      {nonEmptyField ? (
+        <span className={connectStyles.feedback}>
+          <Typing
+            speed={200}
+            text={typingText}
+            typingDelay={0}
+            eraseDelay={200}
+            eraseSpeed={150}
+            cursor=" "
+          />
+        </span>
+      ) : required ? (
+        <span
+          className={`${connectStyles.requiredField} ${
+            focusedField ? connectStyles.animateFieldOption : ""
+          }`}
+        >
+          {" "}
+          Required
+        </span>
+      ) : (
+        <span
+          className={`${connectStyles.optionalField} ${
+            focusedField ? connectStyles.animateFieldOption : ""
+          }`}
+        >
+          Optional
+        </span>
+      )}
+    </label>
+  );
+}
+
+export { Label };
