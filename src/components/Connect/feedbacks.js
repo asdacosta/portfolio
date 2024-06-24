e479f7ae2924736a65486c5557a8d5d63c16bb3e
asdacosta <asdacosta18@gmail.com>
2024-06-24 00:58:02 +0000

    Make feedbacks concise to avoid overlapping to next line

--- src/components/Connect/feedbacks.js
+++ src/components/Connect/feedbacks.js
@@ -1,21 +1,22 @@
 const nameFeedbacks = {
   onInput: [
-    "You may, but using full name isn't necessary.",
+    "No need to use full name.",
     "Name is lengthy.",
     "Numbers not allowed.",
-    "Special characters not allowed.",
+    "Special chars not allowed.",
   ],
-  onBlur: ["Name must be at least 2 characters.", "Use a valid name or none."],
+  onBlur: ["Name must be at least 2 chars.", "Use a valid name or none."],
 };
 const countryFeedback = { onBlur: ["Select a valid country."] };
 const motiveFeedback = {
-  onBlur: ["Select a motive or custom one with 'Other'"],
+  onBlur: ["Select a motive or custom one."],
 };
 const noteFeedback = {
-  onBlur: ["Note must be at least 10 characters.", "Note can't be empty."],
+  onBlur: ["Note must be at least 10 chars.", "Note can't be empty."],
 };
 const mailFeedback = {
-  onInput: ["Email is lengthy. Use a valid email.", "Use a valid email."],
+  onInput: ["Email is lengthy, use a valid one."],
+  onBlur: ["Use a valid email."],
 };
 const submitFeedback = ["Complete the form.", "Send"];
 
