8ff3aa2eaf3720831800959c08014810d3995412
asdacosta <asdacosta18@gmail.com>
2024-06-25 23:55:25 +0000

    Include custom dropdown default to replace default one in datalist

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -315,6 +315,11 @@ function Connect() {
               onBlur={handleBlur}
               onInput={handleInput}
             />
+            <div className={connectStyles.chevronCover}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+              </svg>
+            </div>
             <datalist id="allMotives">
               <option value="Hire short term" />
               <option value="Hire long term" />
