1a7a92f83661100b1d1f9eb5f8b958cf8b188e1f
asdacosta <asdacosta18@gmail.com>
2024-06-26 23:02:51 +0000

    Add onBlur feedback to Motive field

--- src/components/Connect/feedbacks.js
+++ src/components/Connect/feedbacks.js
@@ -9,6 +9,7 @@ const name = {
 };
 const motive = {
   onInput: ["Kindly keep the subject short."],
+  onBlur: ["Name must be at least 2 chars."],
 };
 const note = {
   onBlur: ["Note must be at least 10 chars."],
