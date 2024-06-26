2f7710927b9252e8d729d398814d6f9b998c5b55
asdacosta <asdacosta18@gmail.com>
2024-06-26 23:09:39 +0000

    Add checks props to display a svg if field has no error

--- src/components/Connect/Label.jsx
+++ src/components/Connect/Label.jsx
@@ -12,49 +12,60 @@ function Label({
   required,
   id,
   contentError,
+  check,
 }) {
   return (
     <label htmlFor={id}>
       {inputLabel}{" "}
-      {nonEmptyField && focusedField ? (
-        <span className={connectStyles.feedback}>
-          {!contentError ? (
-            <Typing
-              speed={150}
-              text={typingText}
-              typingDelay={0}
-              eraseDelay={200}
-              eraseSpeed={60}
-              cursor=" "
-            />
-          ) : (
-            <Typing
-              speed={50}
-              text={typingText}
-              typingDelay={0}
-              eraseDelay={2500}
-              eraseSpeed={25}
-              cursor=" "
-            />
-          )}
-        </span>
-      ) : required ? (
-        <span
-          className={`${connectStyles.requiredField} ${
-            focusedOption ? connectStyles.animateFieldOption : ""
-          }`}
-        >
-          {" "}
-          Required
-        </span>
+      {!check ? (
+        nonEmptyField && focusedField ? (
+          <span className={connectStyles.feedback}>
+            {!contentError ? (
+              <Typing
+                speed={150}
+                text={typingText}
+                typingDelay={0}
+                eraseDelay={200}
+                eraseSpeed={60}
+                cursor=" "
+              />
+            ) : (
+              <Typing
+                speed={50}
+                text={typingText}
+                typingDelay={0}
+                eraseDelay={2500}
+                eraseSpeed={25}
+                cursor=" "
+              />
+            )}
+          </span>
+        ) : required ? (
+          <span
+            className={`${connectStyles.requiredField} ${
+              focusedOption ? connectStyles.animateFieldOption : ""
+            }`}
+          >
+            {" "}
+            Required
+          </span>
+        ) : (
+          <span
+            className={`${connectStyles.optionalField} ${
+              focusedOption ? connectStyles.animateFieldOption : ""
+            }`}
+          >
+            Optional
+          </span>
+        )
       ) : (
-        <span
-          className={`${connectStyles.optionalField} ${
-            focusedOption ? connectStyles.animateFieldOption : ""
-          }`}
+        <svg
+          className={connectStyles.checkIcon}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
         >
-          Optional
-        </span>
+          <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+        </svg>
       )}
     </label>
   );
