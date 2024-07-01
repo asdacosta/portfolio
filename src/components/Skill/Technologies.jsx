06fd630222b4ce3602e61ca6bc5bc298ce432a53
asdacosta <asdacosta18@gmail.com>
2024-07-01 12:39:07 +0000

    Reduce vertical value of animation for consistency

--- src/components/Skill/Technologies.jsx
+++ src/components/Skill/Technologies.jsx
@@ -19,7 +19,7 @@ function Technologies() {
 
   const techVariants = {
     visible: { opacity: 1, y: 0, transition: { duration: 1.5 } },
-    hidden: { opacity: 0, y: 150 },
+    hidden: { opacity: 0, y: 100 },
   };
 
   return (
