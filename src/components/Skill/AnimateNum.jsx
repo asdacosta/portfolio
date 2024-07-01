a4dca8920a1cd57c86c8e328cec63a06a8143f98
asdacosta <asdacosta18@gmail.com>
2024-07-01 13:32:39 +0000

    Add delayTime props for components that require delay time

--- src/components/Skill/AnimateNum.jsx
+++ src/components/Skill/AnimateNum.jsx
@@ -8,13 +8,13 @@ import {
 } from "framer-motion";
 import { useEffect, useState } from "react";
 
-function AnimatedNum({ target, percent = true }) {
+function AnimatedNum({ target, percent = true, delayTime = 0 }) {
   const motionValue = useMotionValue(0);
   const number = useTransform(motionValue, (value) => Math.floor(value));
   const [currentValue, setCurrentValue] = useState(0);
 
   useEffect(() => {
-    animate(motionValue, target, { duration: 2.5 });
+    animate(motionValue, target, { duration: 2.5, delay: delayTime });
   }, [target, motionValue]);
 
   useMotionValueEvent(number, "change", (latest) => {
