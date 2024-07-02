dbcacc1eee169a9c62dd4c7bcf2da8703edbcfaf
asdacosta <asdacosta18@gmail.com>
2024-07-02 12:20:59 +0000

    Trigger highlight of About page when in view

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,26 @@
+import { useContext, useEffect, useRef } from "react";
 import aboutStyles from "./About.module.css";
 import { AboutMe } from "./AboutMe";
 import { AboutSocials } from "./AboutSocials";
+import { useAnimation, useInView, motion } from "framer-motion";
+import { MenuContext } from "../../App";
 
 function About() {
+  const aboutRef = useRef(null);
+  const inView = useInView(aboutRef);
+  const { page, setPage } = useContext(MenuContext);
+
+  useEffect(() => {
+    if (inView) {
+      setPage("about");
+    }
+  }, [inView]);
+
   return (
-    <section className={aboutStyles.about}>
+    <motion.section className={aboutStyles.about} ref={aboutRef}>
       <AboutMe />
       <AboutSocials />
-    </section>
+    </motion.section>
   );
 }
 
