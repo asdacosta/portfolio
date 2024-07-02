940e9ecd3b9505165e274575d157d2d3d02bd826
asdacosta <asdacosta18@gmail.com>
2024-07-02 12:22:01 +0000

    Trigger highlight of Skill page when in view

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,3 +1,4 @@
+import { useContext, useEffect, useRef } from "react";
 import { AboutSocials } from "../About/AboutSocials";
 import { AllDevices } from "./AllDevices";
 import { Analytics } from "./Analytics";
@@ -9,10 +10,22 @@ import { Research } from "./Research";
 import skillStyles from "./Skill.module.css";
 import { Team } from "./Team";
 import { Technologies } from "./Technologies";
+import { MenuContext } from "../../App";
+import { useInView } from "framer-motion";
 
 function Skill() {
+  const { page, setPage } = useContext(MenuContext);
+  const SkillRef = useRef(null);
+  const inView = useInView(SkillRef);
+
+  useEffect(() => {
+    if (inView) {
+      setPage("skill");
+    }
+  }, [inView]);
+
   return (
-    <section className={skillStyles.skill}>
+    <section className={skillStyles.skill} ref={SkillRef}>
       <AllDevices />
       <Mobile />
       <Analytics />
