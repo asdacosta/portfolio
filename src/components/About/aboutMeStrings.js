0109e14583b4cecb89cb9de81e75ee1dd6c319de
asdacosta <asdacosta18@gmail.com>
2024-07-03 16:21:07 +0000

    Add array of strings used in AboutMe.jsx

---
+++ src/components/About/aboutMeStrings.js
@@ -0,0 +1,12 @@
+const text = ["Web Developer.", "UI/UX Designer.", "Web Analyst.", "Linguist."];
+
+const infos = [
+  "I develop interactive websites that are responsive across various devices, ensuring seamless user interfaces in collaboration with back-end developers.",
+  "I design visually appealing and user-friendly websites, focusing on layout, aesthetics, and refining user experience through continuous improvement.",
+  "With a background in Statistics, I leverage my analytical and problem solving skills to solve complex challenges in development, ensuring optimal solutions.",
+  "With fluency in four languages, I craft websites to suit users with diverse cultural nuances and collaborate well with teams having various cultural backgrounds.",
+];
+
+const aboutMeStrings = { text, infos };
+
+export { aboutMeStrings };
