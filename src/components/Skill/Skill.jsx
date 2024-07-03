6231b916c482435bcf4d2b84645616274f6c4f16
asdacosta <asdacosta18@gmail.com>
2024-07-03 21:21:34 +0000

    Import cards as a whole instead

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,17 +1,11 @@
 import { useContext, useEffect, useRef } from "react";
-import { AllDevices } from "./Cards/AllDevices";
-import { Analytics } from "./Cards/Analytics";
-import { Author } from "./Cards/Author";
-import { Bot } from "./Cards/Bot";
 import { Education } from "./Education";
-import { Mobile } from "./Cards/Mobile";
-import { Research } from "./Cards/Research";
 import skillStyles from "./Skill.module.css";
-import { Team } from "./Cards/Team";
 import { Technologies } from "./Technologies/Technologies";
 import { MenuContext } from "../../App";
 import { useInView } from "framer-motion";
 import { Element } from "react-scroll";
+import { Cards } from "./Cards/Cards";
 
 function Skill() {
   const { page, setPage } = useContext(MenuContext);
@@ -27,13 +21,7 @@ function Skill() {
   return (
     <section className={skillStyles.skill} ref={SkillRef}>
       <Element name="Skill" className="targetScroll"></Element>
-      <AllDevices />
-      <Mobile />
-      <Analytics />
-      <Bot />
-      <Team />
-      <Author />
-      <Research />
+      <Cards />
       <Education />
       <Technologies />
     </section>
