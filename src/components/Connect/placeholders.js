1aa67cf102be7646e8c4e5ac195151498d52cef8
asdacosta <asdacosta18@gmail.com>
2024-07-03 05:05:00 +0000

    Add extra line at the end of message

--- src/components/Connect/placeholders.js
+++ src/components/Connect/placeholders.js
@@ -25,14 +25,14 @@ const mails = [
 ];
 
 const notes = [
-  "Hi Ace, \nI am interested in your services for... ^1000 . Could you provide more information about your availability and pricing? \nBest regards.",
-  "Hey Ace, \nI’m... ^1000 at... ^1000 , and we’re currently looking for a developer to join our team. I believe you'd be a great fit. Interested in discussing this opportunity? \nSincerely.",
-  "Hello Ace, \nI came across your portfolio and was impressed with your work. As someone in the same field, I’d love to connect. \nBest.",
-  "Hello Ace, \nI'm impressed with your work and would like to offer financial support to help you continue creating such works. How can I contribute? \nBest.",
-  "Good morning Ace, \nI recently saw your portfolio and was impressed. I'd love to learn more about your process and upcoming projects. Can we schedule a chat? \nRegards.",
-  "Greetings Ace, \nI am working on a project that I believe aligns well with your skills. Would you be interested in discussing a potential collaboration? \nLooking forward to your response.",
-  "Dear Ace, \nI just wanted to reach out and say how much I admire your work, especially... ^1000 . Your attention to detail and creativity are great. Keep it up! \nThank you.",
-  "Hi Ace, \nI hope you're well. I wanted to bring to your attention a bug I encountered on your portfolio website. The issue occurs when... ^1000 . \nThank you.",
+  "Hi Ace, \nI am interested in your services for... ^1000 . Could you provide more information about your availability and pricing? \n\nBest regards.",
+  "Hey Ace, \nI’m... ^1000 at... ^1000 , and we’re currently looking for a developer to join our team. I believe you'd be a great fit. Interested in discussing this opportunity? \n\nSincerely.",
+  "Hello Ace, \nI came across your portfolio and was impressed with your work. As someone in the same field, I’d love to connect. \n\nBest.",
+  "Hello Ace, \nI'm impressed with your work and would like to offer financial support to help you continue creating such works. How can I contribute? \n\nBest.",
+  "Good morning Ace, \nI recently saw your portfolio and was impressed. I'd love to learn more about your process and upcoming projects. Can we schedule a chat? \n\nRegards.",
+  "Greetings Ace, \nI am working on a project that I believe aligns well with your skills. Would you be interested in discussing a potential collaboration? \n\nLooking forward to your response.",
+  "Dear Ace, \nI just wanted to reach out and say how much I admire your work, especially... ^1000 . Your attention to detail and creativity are great. Keep it up! \n\nThank you.",
+  "Hi Ace, \nI hope you're well. I wanted to bring to your attention a bug I encountered on your portfolio website. The issue occurs when... ^1000 . \n\nThank you.",
   "Hi Ace, ...^3000",
 ];
 
