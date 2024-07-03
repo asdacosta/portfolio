e3259039c15557a29d6406869a953ac147dac9e0
asdacosta <asdacosta18@gmail.com>
2024-07-03 01:31:59 +0000

    Append framer motion icon

--- src/components/Skill/FrontSection.jsx
+++ src/components/Skill/FrontSection.jsx
@@ -33,6 +33,16 @@ function FrontSection() {
           </svg>
         </div>
       </div>
+      <div className={skillStyles.iconContainer}>
+        <div className={`${skillStyles.framerBox} ${skillStyles.box}`}>
+          <svg viewBox="0 0 128 128">
+            <path
+              fill="#FFFFFF"
+              d="M22.684 0h84.253v42.667H64.81L22.684 0Zm0 42.667H64.81l42.127 42.666H64.81V128L22.684 85.333V42.667Z"
+            ></path>
+          </svg>
+        </div>
+      </div>
     </section>
   );
 }
