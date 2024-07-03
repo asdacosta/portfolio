03123cad1e76ffd91acc88deb87b2b91655ec47d
asdacosta <asdacosta18@gmail.com>
2024-07-03 00:40:57 +0000

    Add links active socials

--- src/components/About/AboutSocials.jsx
+++ src/components/About/AboutSocials.jsx
@@ -214,6 +214,9 @@ function AboutSocials() {
           key="github"
           variants={iconVariant}
           className={aboutStyles.githubBox}
+          onClick={() => {
+            window.open("https://github.com/asdacosta", "_blank");
+          }}
         >
           <svg
             ref={githubRef}
@@ -234,6 +237,9 @@ function AboutSocials() {
           key="linkedIn"
           variants={iconVariant}
           className={aboutStyles.linkedInBox}
+          onClick={() => {
+            window.open("https://bit.ly/aceCosta", "_blank");
+          }}
         >
           <svg
             ref={linkedInRef}
@@ -250,7 +256,14 @@ function AboutSocials() {
             <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
           </svg>
         </motion.div>
-        <motion.div key="X" variants={iconVariant} className={aboutStyles.xBox}>
+        <motion.div
+          key="X"
+          variants={iconVariant}
+          className={aboutStyles.xBox}
+          onClick={() => {
+            window.open("https://x.com/Ace_DaCosta", "_blank");
+          }}
+        >
           <svg
             ref={xRef}
             onMouseEnter={blurOthers}
@@ -270,6 +283,9 @@ function AboutSocials() {
           key="ig"
           variants={iconVariant}
           className={aboutStyles.igBox}
+          onClick={() => {
+            window.open("https://www.instagram.com/ace_dacosta/", "_blank");
+          }}
         >
           <svg
             ref={igRef}
@@ -330,6 +346,9 @@ function AboutSocials() {
           key="medium"
           variants={iconVariant}
           className={aboutStyles.mediumBox}
+          onClick={() => {
+            window.open("https://medium.com/@dacostasilvanus", "_blank");
+          }}
         >
           <svg
             ref={mediumRef}
