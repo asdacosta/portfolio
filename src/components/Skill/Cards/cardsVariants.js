52107dd67d899a6e1ce928674576587685395624
asdacosta <asdacosta18@gmail.com>
2024-07-03 21:12:28 +0000

    Add variants for Cards

---
+++ src/components/Skill/Cards/cardsVariants.js
@@ -0,0 +1,13 @@
+const leftVariant = {
+  visible: { opacity: 1, x: 0, transition: { duration: 1.5 } },
+  hidden: { opacity: 0, x: -150 },
+};
+
+const rightVariant = {
+  visible: { opacity: 1, x: 0, transition: { duration: 1.5 } },
+  hidden: { opacity: 0, x: 150 },
+};
+
+const cardsVariants = { leftVariant, rightVariant };
+
+export { cardsVariants };
