b2361b119e09c77b7b589e9e840ada202b2b34f5
asdacosta <asdacosta18@gmail.com>
2024-07-03 03:31:53 +0000

    Define placeholder templates for each field

---
+++ src/components/Connect/placeholders.js
@@ -0,0 +1,37 @@
+const name = ["Ace Da Costa", "Enter name..."];
+
+const motive = [
+  "Hire short term",
+  "Hire long term",
+  "Consult",
+  "Collaborate",
+  "General Inquiry",
+  "Bug Report",
+  "Support",
+  "Custom or select subject...",
+];
+
+const mail = [
+  "ace@example.com",
+  "ace.costa@example.com",
+  "ace-costa@example.com",
+  "ace_costa@example.com",
+  "ace2025@example.com",
+  "ace@yahoo.com",
+  "ace@outlook.com",
+  "ace@icloud.com",
+  "ace@aol.com",
+  "ace@university.edu",
+];
+
+const note = [
+  "Hi Ace, \n I am interested in your services for... . Could you provide more information about your availability and pricing? \n Best regards.",
+  "Hey Ace, \n I’m... at... , and we’re currently looking for a developer to join our team. I believe you'd be a great fit. Interested in discussing this opportunity? \n Sincerely.",
+  "Hello Ace, \n I came across your portfolio and was impressed with your work. As someone in the same field, I’d love to connect. \n Best.",
+  "Hello Ace, \n I'm impressed with your work and would like to offer financial support to help you continue creating such works. How can I contribute? \n Best.",
+  "Good morning Ace, \n I recently saw your portfolio and was impressed. I'd love to learn more about your process and upcoming projects. Can we schedule a chat? \n Regards.",
+  "Greetings Ace, \n I am working on a project that I believe aligns well with your skills. Would you be interested in discussing a potential collaboration? \n Looking forward to your response.",
+  "Dear Ace, \n I just wanted to reach out and say how much I admire your work, especially... . Your attention to detail and creativity are great. Keep it up! \n Thank you.",
+  "Hi Ace, \n I hope you're well. I wanted to bring to your attention a bug I encountered on your portfolio website. The issue occurs when... . \n Thank you.",
+  "Hi Ace, ...",
+];
