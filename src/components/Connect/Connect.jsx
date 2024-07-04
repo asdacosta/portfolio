25692a9d637c4a918879f5fc9fe81eef05e08c8e
asdacosta <asdacosta18@gmail.com>
2024-07-04 14:57:18 +0000

    Create motive box to help chevron be bounded by it when positioned absolute

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -608,22 +608,24 @@ function Connect() {
               contentError={contentErrors.motive.isError}
               check={checks.motive}
             />
-            <input
-              list="allMotives"
-              name="motive"
-              id="motive"
-              placeholder="Custom or select..."
-              maxLength="31"
-              required
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-              onInput={handleInput}
-              ref={typingMotiveRef}
-            />
-            <div className={connectStyles.chevronCover}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-              </svg>
+            <div className={connectStyles.motiveInputBox}>
+              <input
+                list="allMotives"
+                name="motive"
+                id="motive"
+                placeholder="Custom or select..."
+                maxLength="31"
+                required
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                onInput={handleInput}
+                ref={typingMotiveRef}
+              />
+              <div className={connectStyles.chevronCover}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                </svg>
+              </div>
             </div>
             <datalist id="allMotives">
               <option value="Hire short term" />
