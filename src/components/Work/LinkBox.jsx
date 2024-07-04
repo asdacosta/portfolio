2001a14dcd20046bca73c67495cf7bb1ed838e9f
asdacosta <asdacosta18@gmail.com>
2024-07-04 12:59:00 +0000

    Use svg instead of emoji for consistency across all screens

--- src/components/Work/LinkBox.jsx
+++ src/components/Work/LinkBox.jsx
@@ -76,16 +76,28 @@ function LinkBox({ revealBox, technologies, links }) {
             <motion.span key="first1" variants={textVariant}>
               {technologies[0]}
             </motion.span>
-            <motion.span key="sec1" variants={dotVariant}>
-              ⚫
+            <motion.span
+              key="sec1"
+              variants={dotVariant}
+              className={workStyles.dotBox}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
+              </svg>
             </motion.span>
             <motion.span key="third1" variants={textVariant}>
               {technologies[1]}
             </motion.span>
             {technologies[2] !== null ? (
               <>
-                <motion.span key="fourth1" variants={dotVariant}>
-                  ⚫
+                <motion.span
+                  key="fourth1"
+                  variants={dotVariant}
+                  className={workStyles.dotBox}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
+                  </svg>
                 </motion.span>
                 <motion.span key="fifth1" variants={textVariant}>
                   {technologies[2]}
