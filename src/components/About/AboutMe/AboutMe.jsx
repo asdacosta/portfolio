19a2b058a29eb23b259197ee88a698ab20fe2c3f
asdacosta <asdacosta18@gmail.com>
2024-07-04 23:12:24 +0000

    Indicate downloaded to trigger CSS style

--- src/components/About/AboutMe/AboutMe.jsx
+++ src/components/About/AboutMe/AboutMe.jsx
@@ -15,6 +15,7 @@ function AboutMe() {
   const [slideUpNow, setSlideUpNow] = useState(false);
   const [hideCursor, setHideCursor] = useState(false);
   const [fillProgress, setFillProgress] = useState(false);
+  const [resumeDownloaded, setResumeDownloaded] = useState(false);
 
   const aboutMeRef = useRef(null);
   const helloRef = useRef(null);
@@ -71,6 +72,11 @@ function AboutMe() {
     }
   };
   useEffect(slideUpWhenTypingLapses, [slideUpNow]);
+
+  const indicateResumeDownloaded = () => {
+    setResumeDownloaded(true);
+    setTimeout(() => setResumeDownloaded(false), 2000);
+  };
 
   return (
     <motion.section
@@ -153,7 +159,10 @@ function AboutMe() {
       <motion.button
         key="resume"
         variants={aboutMeVariants.buttonVariant}
-        className={aboutStyles.resume}
+        className={`${aboutStyles.resume} ${
+          resumeDownloaded ? aboutStyles.downloaded : ""
+        }`}
+        onClick={indicateResumeDownloaded}
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
           <path d="M320 464c8.8 0 16-7.2 16-16V160H256c-17.7 0-32-14.3-32-32V48H64c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320zM0 64C0 28.7 28.7 0 64 0H229.5c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64z" />
