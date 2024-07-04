231fc098362859abef1582011b1d4ac7b88cbab9
asdacosta <asdacosta18@gmail.com>
2024-07-04 14:41:59 +0000

    Remove var from closure to allow external functions to use

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -407,8 +407,8 @@ function Connect() {
       }
     })();
 
+    const mailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
     const validateMail = (() => {
-      const mailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
       if (inputType === "mail" && inputValue.length > 0) {
         if (!mailRegex.test(inputValue)) {
           setContentErrors((prev) => ({
