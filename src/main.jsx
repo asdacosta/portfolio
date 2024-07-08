3af10517329e8c45ea457c665646cccbd2c79ca0
asdacosta <asdacosta18@gmail.com>
2024-07-08 13:58:09 +0000

    Route to respective components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ErrorPage } from "./components/ErrorPage/ErrorPage.jsx";
+import { ConnectFeedback } from "./components/Connect/ConnectFeedback/ConnectFeedback.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [{ path: "feedback", element: <ConnectFeedback /> }],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
