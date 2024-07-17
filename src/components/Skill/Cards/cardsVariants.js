8863c211b811cb5dc0f9e2b77175736a1a7e22bd
asdacosta <asdacosta18@gmail.com>
2024-07-17 12:29:57 +0000

    Reduce card timings from faster animation

--- src/components/Skill/Cards/cardsVariants.js
+++ src/components/Skill/Cards/cardsVariants.js
@@ -1,10 +1,10 @@
 const leftVariant = {
-  visible: { opacity: 1, x: 0, transition: { duration: 1.5 } },
+  visible: { opacity: 1, x: 0, transition: { duration: 1.3 } },
   hidden: { opacity: 0, x: -150 },
 };
 
 const rightVariant = {
-  visible: { opacity: 1, x: 0, transition: { duration: 1.5 } },
+  visible: { opacity: 1, x: 0, transition: { duration: 1.3 } },
   hidden: { opacity: 0, x: 150 },
 };
 
