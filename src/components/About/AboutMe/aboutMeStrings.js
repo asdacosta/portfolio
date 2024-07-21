d16c8c2642b1c4f0ec061e68ff5fd60d2234665c
asdacosta <asdacosta18@gmail.com>
2024-07-21 11:19:55 +0000

    Include info on AI technologies

--- src/components/About/AboutMe/aboutMeStrings.js
+++ src/components/About/AboutMe/aboutMeStrings.js
@@ -1,14 +1,14 @@
 const expertise = [
   "Web Developer.",
   "UI/UX Designer.",
-  "Web Analyst.",
+  "AI Expert.",
   "Linguist.",
 ];
 
 const info = [
   "I develop interactive websites that are responsive across various devices, ensuring seamless user interfaces in collaboration with back-end developers.",
   "I design visually appealing and user-friendly websites, focusing on layout, aesthetics, and refining user experience through continuous improvement.",
-  "With a background in Statistics, I leverage my analytical and problem solving skills to solve complex challenges in development, ensuring optimal solutions.",
+  "I leverage AI technologies to build efficient websites and automate tasks. Also, my background in Statistics aids in applying my analytical kills for optimal solutions.",
   "With fluency in four languages, I craft websites to suit users with diverse cultural nuances and collaborate well with teams having various cultural backgrounds.",
 ];
 
