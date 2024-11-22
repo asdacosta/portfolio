94e84a1d838e4708fd82f6fa2532c0393d51d377
asdacosta <asdacosta18@gmail.com>
2024-11-22 13:01:45 +0000

    Alter strings

--- src/components/About/AboutMe/aboutMeStrings.js
+++ src/components/About/AboutMe/aboutMeStrings.js
@@ -1,14 +1,14 @@
 const expertise = [
-  "Web Developer.",
+  "Full-stack Developer.",
   "UI/UX Designer.",
   "AI Expert.",
   "Linguist.",
 ];
 
 const info = [
-  "I develop interactive websites that are responsive across various devices, ensuring seamless user interfaces in collaboration with back-end developers.",
+  "I develop and maintain complete websites, managing everything from front-end design to back-end systems, ensuring they are interactive, responsive, and provide an optimal user experience.",
   "I design visually appealing and user-friendly websites, focusing on layout, aesthetics, and refining user experience through continuous improvement.",
-  "I leverage AI technologies to build efficient websites and automate tasks. Also, my background in Statistics aids in applying my analytical kills for optimal solutions.",
+  "I leverage AI technologies to build efficient websites and automate tasks. Also, my background in Statistics aids in applying my analytical skill for optimal solutions.",
   "With fluency in four languages, I craft websites to suit users with diverse cultural nuances and collaborate well with teams having various cultural backgrounds.",
 ];
 
