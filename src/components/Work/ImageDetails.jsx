14708584125651d8d2617f5568100d243c30e540
asdacosta <asdacosta18@gmail.com>
2024-11-22 20:20:35 +0000

    Rearrange details

--- src/components/Work/ImageDetails.jsx
+++ src/components/Work/ImageDetails.jsx
@@ -46,21 +46,6 @@ const images = [
     ),
   },
   {
-    src: messageBoardImg,
-    alt: "Message Board",
-    technologies: ["Express", "PostgreSQL", "EJS"],
-    links: {
-      live: "https://selected-gussy-githubprojects-5d8d6159.koyeb.app/",
-      code: "https://github.com/asdacosta/messageboard",
-    },
-    index: "thirdImg",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-        <path d="M64 0C28.7 0 0 28.7 0 64L0 352c0 35.3 28.7 64 64 64l96 0 0 80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416 448 416c35.3 0 64-28.7 64-64l0-288c0-35.3-28.7-64-64-64L64 0z" />
-      </svg>
-    ),
-  },
-  {
     src: resumeImg,
     alt: "Resume Builder",
     technologies: ["React", "CSS", "Vite"],
@@ -68,7 +53,7 @@ const images = [
       live: "https://resume-nu-lake.vercel.app/",
       code: "https://github.com/asdacosta/resume",
     },
-    index: "fourthImg",
+    index: "thirdImg",
     icon: (
       <svg
         className={workStyles.resumeIcon}
@@ -87,7 +72,7 @@ const images = [
       live: "https://asdacosta.github.io/battleship/",
       code: "https://github.com/asdacosta/battleship",
     },
-    index: "fifthImg",
+    index: "fourthImg",
     icon: (
       <svg
         className={workStyles.gameIcon}
@@ -106,7 +91,7 @@ const images = [
       live: "https://inventory-production-d169.up.railway.app/",
       code: "https://github.com/asdacosta/inventory",
     },
-    index: "sixthImg",
+    index: "fifthImg",
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
         <path d="M253.3 35.1c6.1-11.8 1.5-26.3-10.2-32.4s-26.3-1.5-32.4 10.2L117.6 192 32 192c-17.7 0-32 14.3-32 32s14.3 32 32 32L83.9 463.5C91 492 116.6 512 146 512L430 512c29.4 0 55-20 62.1-48.5L544 256c17.7 0 32-14.3 32-32s-14.3-32-32-32l-85.6 0L365.3 12.9C359.2 1.2 344.7-3.4 332.9 2.7s-16.3 20.6-10.2 32.4L404.3 192l-232.6 0L253.3 35.1zM192 304l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16s16 7.2 16 16zm96-16c8.8 0 16 7.2 16 16l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16zm128 16l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16s16 7.2 16 16z" />
@@ -121,7 +106,7 @@ const images = [
       live: "https://membersonly-production-ad85.up.railway.app/",
       code: "https://github.com/asdacosta/membersonly",
     },
-    index: "seventhImg",
+    index: "sixthImg",
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
         <path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192l42.7 0c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0L21.3 320C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7l42.7 0C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3l-213.3 0zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352l117.3 0C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7l-330.7 0c-14.7 0-26.7-11.9-26.7-26.7z" />
@@ -136,7 +121,7 @@ const images = [
       live: "https://asdacosta.github.io/weather-forecast/",
       code: "https://github.com/asdacosta/weather-forecast",
     },
-    index: "eightImg",
+    index: "seventhImg",
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
         <path d="M0 224c0 53 43 96 96 96h47.2L290 202.5c17.6-14.1 42.6-14 60.2 .2s22.8 38.6 12.8 58.8L333.7 320H352h64c53 0 96-43 96-96s-43-96-96-96c-.5 0-1.1 0-1.6 0c1.1-5.2 1.6-10.5 1.6-16c0-44.2-35.8-80-80-80c-24.3 0-46.1 10.9-60.8 28C256.5 24.3 219.1 0 176 0C114.1 0 64 50.1 64 112c0 7.1 .7 14.1 1.9 20.8C27.6 145.4 0 181.5 0 224zm330.1 3.6c-5.8-4.7-14.2-4.7-20.1-.1l-160 128c-5.3 4.2-7.4 11.4-5.1 17.8s8.3 10.7 15.1 10.7h70.1L177.7 488.8c-3.4 6.7-1.6 14.9 4.3 19.6s14.2 4.7 20.1 .1l160-128c5.3-4.2 7.4-11.4 5.1-17.8s-8.3-10.7-15.1-10.7H281.9l52.4-104.8c3.4-6.7 1.6-14.9-4.2-19.6z" />
@@ -151,7 +136,7 @@ const images = [
       live: "https://asdacosta.github.io/todo-list/",
       code: "https://github.com/asdacosta/todo-list",
     },
-    index: "ninthImg",
+    index: "eightImg",
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
         <path d="M64 144a48 48 0 1 0 0-96 48 48 0 1 0 0 96zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM64 464a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm48-208a48 48 0 1 0 -96 0 48 48 0 1 0 96 0z" />
@@ -166,7 +151,7 @@ const images = [
       live: "https://asdacosta.github.io/tic-tac-toe-game/",
       code: "https://github.com/asdacosta/tic-tac-toe-game",
     },
-    index: "tenthImg",
+    index: "ninthImg",
     icon: (
       <svg
         className={workStyles.gameIcon}
@@ -185,7 +170,7 @@ const images = [
       live: "https://memory-card-snowy-three.vercel.app/",
       code: "https://github.com/asdacosta/memory-card",
     },
-    index: "eleventhImg",
+    index: "tenthImg",
     icon: (
       <svg
         className={workStyles.gameIcon}
@@ -193,6 +178,21 @@ const images = [
         viewBox="0 0 640 512"
       >
         <path d="M192 64C86 64 0 150 0 256S86 448 192 448H448c106 0 192-86 192-192s-86-192-192-192H192zM496 168a40 40 0 1 1 0 80 40 40 0 1 1 0-80zM392 304a40 40 0 1 1 80 0 40 40 0 1 1 -80 0zM168 200c0-13.3 10.7-24 24-24s24 10.7 24 24v32h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H216v32c0 13.3-10.7 24-24 24s-24-10.7-24-24V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h32V200z" />
+      </svg>
+    ),
+  },
+  {
+    src: messageBoardImg,
+    alt: "Message Board",
+    technologies: ["Express", "PostgreSQL", "EJS"],
+    links: {
+      live: "https://selected-gussy-githubprojects-5d8d6159.koyeb.app/",
+      code: "https://github.com/asdacosta/messageboard",
+    },
+    index: "eleventhImg",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path d="M64 0C28.7 0 0 28.7 0 64L0 352c0 35.3 28.7 64 64 64l96 0 0 80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416 448 416c35.3 0 64-28.7 64-64l0-288c0-35.3-28.7-64-64-64L64 0z" />
       </svg>
     ),
   },
