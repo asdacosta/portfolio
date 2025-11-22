60439aaf07a4d4ea43f0785178a03c6b70c7a498
asdacosta <asdacosta18@gmail.com>
2025-11-22 07:03:28 +0000

    Update AboutMe component to include downloadable resume link and refine currentInfo text

--- src/components/About/AboutMe/AboutMe.jsx
+++ src/components/About/AboutMe/AboutMe.jsx
@@ -4,11 +4,12 @@ import aboutStyles from "../About.module.css";
 import { useEffect, useRef, useState } from "react";
 import { aboutMeVariants } from "./aboutMeVariants.js";
 import { aboutMeStrings } from "./aboutMeStrings.js";
+import resumePdf from "../../../assets/resume.pdf";
 
 function AboutMe() {
   const [expertise, setExpertise] = useState("");
   const [currentInfo, setCurrentInfo] = useState(
-    "I develop interactive websites that are responsive across various devices, ensuring seamless user interfaces in collaboration with back-end developers."
+    "I develop and maintain complete websites, managing everything from front-end design to back-end systems, ensuring they are interactive, responsive, and provide an optimal user experience."
   );
   const [expertiseCharIndex, setExpertiseCharIndex] = useState(0);
   const [expertiseIndex, setExpertiseIndex] = useState(0);
@@ -156,19 +157,21 @@ function AboutMe() {
           }
         ></p>
       </motion.section>
-      <motion.button
+      <motion.a
         key="resume"
         variants={aboutMeVariants.buttonVariant}
         className={`${aboutStyles.resume} ${
           resumeDownloaded ? aboutStyles.downloaded : ""
         }`}
+        href={resumePdf}
+        download="Ace_Da_Costa_Resume.pdf"
         onClick={indicateResumeDownloaded}
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
           <path d="M320 464c8.8 0 16-7.2 16-16V160H256c-17.7 0-32-14.3-32-32V48H64c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320zM0 64C0 28.7 28.7 0 64 0H229.5c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64z" />
         </svg>
         Resume
-      </motion.button>
+      </motion.a>
     </motion.section>
   );
 }
