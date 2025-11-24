14d3bdcc998856b4bf212f15ef3b76f42e96e067
asdacosta <asdacosta18@gmail.com>
2025-11-24 09:10:19 +0000

    Replace the SVG attributes that use hyphenated names with their React/JSX camelCase equivalents.

--- src/components/Skill/Technologies/Tools.jsx
+++ src/components/Skill/Technologies/Tools.jsx
@@ -25,8 +25,8 @@ const ToolSection = React.memo(function ToolSection() {
                 gradientTransform="scale(.71111)"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop stop-color="#fff"></stop>
-                <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                <stop stopColor="#fff"></stop>
+                <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
               </linearGradient>
               <linearGradient
                 id="b"
@@ -37,8 +37,8 @@ const ToolSection = React.memo(function ToolSection() {
                 gradientTransform="scale(.71111)"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop stop-color="#fff"></stop>
-                <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                <stop stopColor="#fff"></stop>
+                <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
               </linearGradient>
             </defs>
           </svg>
